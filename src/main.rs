3df6ff719e2675a4398fa7ec2aefbdc85357bf2c
kmate <k_mate@inf.elte.hu>
2017-08-12 02:30:25 +0200

    Argument parsing and IO

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,74 @@
+extern crate clap;
 #[macro_use]
 extern crate lazy_static;
 extern crate regex;
+
+use clap::{Arg, App};
+use std::error::Error;
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
 
 mod codegen;
 mod inst;
 mod parser;
 mod symtab;
 
+fn existing_file(path: String) -> Result<(), String> {
+    let info = fs::metadata(path).map_err(|e| e.description().to_string())?;
+    if info.is_file() {
+        Ok(())
+    } else {
+        Err(String::from("input file does not exist"))
+    }
+}
+
+fn read_input(input_option: Option<&str>) -> io::Result<String> {
+    let mut buffer = String::new();
+    if let Some(path) = input_option {
+        let mut file = File::open(path)?;
+        file.read_to_string(&mut buffer)?;
+    } else {
+        io::stdin().read_to_string(&mut buffer)?;
+    }
+    Ok(buffer)
+}
+
+fn write_output(output_option: Option<&str>, buffer: String) -> io::Result<()> {
+    if let Some(path) = output_option {
+        let mut file = File::create(path)?;
+        file.write_all(buffer.as_bytes())?;
+    } else {
+        print!("{}", buffer);
+        io::stdout().flush().ok();
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Hack Assembler")
+        .version("1.0")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("Sets the input file to use")
+                .takes_value(true)
+                .validator(existing_file),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Sets the output file to use")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let buffer = read_input(matches.value_of("input")).expect("Input error");
+    // TODO insert parsing and code generation logic
+    write_output(matches.value_of("output"), buffer).expect("Output error");
 }
